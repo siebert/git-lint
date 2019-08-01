1bcae2424290896a1b8e7f1c3170f2d47d0c46ae
Steffen Siebert <steffen.siebert@snom.com>
2019-08-01 09:51:01 +0200

    Filter 'documented symbol `foo' was not declared or defined.' warnings from doxygen output
    We don't know how to fix/prevent these warnings, so we have to filter them.

--- scripts/custom_linters/doxygen-linter.sh
+++ scripts/custom_linters/doxygen-linter.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-( cat $1; echo "INPUT=$2" ) | doxygen -
+
+# As we currently don't know how to prevent the "documented symbol `foo' was not declared or defined." warnings, we filter them.
+( cat $1; echo "INPUT=$2" ) | doxygen - 2>&1 | grep -v "documented symbol \`.*' was not declared or defined."
